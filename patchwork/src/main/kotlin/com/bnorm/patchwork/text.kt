c5c585775d0937f59899ee72ac84af7e0a4b2941
Brian Norman <brian@bnorm.com>
2020-11-12 21:08:00 -0600

    Handle ANSI color characters
     Conflicts:
    	build.gradle.kts
    	patchwork/src/main/kotlin/com/bnorm/patchwork/Patch.kt
    	patchwork/src/main/kotlin/com/bnorm/patchwork/text.kt
    	patchwork/src/main/kotlin/com/bnorm/patchwork/work.kt
    	patchwork/src/test/kotlin/com/bnorm/patchwork/PatchworkTest.kt
    	sample/build.gradle.kts
    	settings.gradle.kts
    	src/commonMain/kotlin/com/bnorm/patchwork/Patch.kt
    	src/commonMain/kotlin/com/bnorm/patchwork/work.kt
    	src/main/kotlin/com/bnorm/patchwork/Patch.kt
    	src/main/kotlin/com/bnorm/patchwork/work.kt

--- patchwork/src/main/kotlin/com/bnorm/patchwork/text.kt
+++ patchwork/src/main/kotlin/com/bnorm/patchwork/text.kt
@@ -1,6 +1,7 @@
 package com.bnorm.patchwork
 
-internal const val ANSI_ESCAPE = "\u001B["
+private const val ANSI_ESCAPE = "\u001B["
+private val ansiColorEscape = Regex("""\u001B\[\d+(;\d+)*m""")
 
 internal fun ansiMoveCursor(vertical: Int, horizontal: Int): String {
   if (vertical == 0 && horizontal == 0) return ""
@@ -19,3 +20,25 @@ internal fun ansiMoveCursor(vertical: Int, horizontal: Int): String {
 }
 
 internal fun ansiClear(): String = "${ANSI_ESCAPE}K"
+
+internal fun String.visibleAt(index: Int): Int {
+  return substring(0, index).visualCodePointCount
+}
+
+internal val CharSequence.visualCodePointCount: Int get() {
+  // Fast path: no escapes.
+  val firstEscape = indexOf('\u001B')
+  if (firstEscape == -1) {
+    return Character.codePointCount(this, 0, length)
+  }
+
+  var currentIndex = firstEscape
+  var count = Character.codePointCount(this, 0, firstEscape)
+  while (true) {
+    val match = ansiColorEscape.find(this, startIndex = currentIndex) ?: break
+    count += Character.codePointCount(this, currentIndex, match.range.first)
+    currentIndex = match.range.last + 1
+  }
+  count += Character.codePointCount(this, currentIndex, length)
+  return count
+}
